9b75996bb9b5b940d73f949022550cfc7946653d
Terr14 <morgus.argh@gmail.com>
2022-02-06 19:11:14 +0300

    Game_v3

--- SF_DataScience_projects/Project 1/game_v3.py
+++ SF_DataScience_projects/Project 1/game_v3.py
@@ -1,13 +1,23 @@
+
 import numpy as np
 
 def random_predict_v3(number:int=1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    
     n_min = 1
     n_max = 101
     count = 0
     number = np.random.randint(n_min, n_max)        
-    while True:
+    while True: # Выбирам число из середины диапазона, пока не угадаем
         count += 1
-        n_mid = round((n_min+n_max)/2)
+        n_mid = round((n_min+n_max)/2) 
         if n_mid > number:
             n_max = n_mid
         
@@ -15,12 +25,11 @@ def random_predict_v3(number:int=1) -> int:
             n_min = n_mid
 
         else:
-            #print(f"Компьютер угадал число за {count} попыток. Это число {number}")
             return count
        
 
 def score_game(random_predict_v3) -> int:
-    """За какое количство попыток в среднем за n подходов угадывает наш алгоритм
+    """За какое количсетво попыток в среднем за n подходов угадывает алгоритм
 
     Args:
         random_predict ([type]): функция угадывания
@@ -29,13 +38,14 @@ def score_game(random_predict_v3) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    n = 100
+    n = 100 # Количество повторений
 
     for number in range(n):
         count_ls.append(random_predict_v3())
 
     score = int(np.mean(count_ls))
-    print(f"Алгоритм угадывает число в среднем за: {score} попыток. Количество повторений: {n}")
+    print(f"Алгоритм угадывает число в среднем за: {score} попыток.\
+        Количество повторений: {n}")
     return score
 
 score_game(random_predict_v3)
