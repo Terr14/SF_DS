7d6a0aef407b0a386b4e330ad5e2ae54590501b5
Terr14 <morgus.argh@gmail.com>
2022-02-09 16:20:57 +0300

    Game_v3_edit

--- SF_DataScience_projects/Project 1/game_v3.py
+++ SF_DataScience_projects/Project 1/game_v3.py
@@ -1,5 +1,4 @@
 import numpy as np
-from numpy import random
 
 def random_predict_v3(number:int=1) -> int:
     """Рандомно угадываем число
@@ -14,32 +13,34 @@ def random_predict_v3(number:int=1) -> int:
     n_min = 1
     n_max = 101
     count = 0
-    predict_number = 1
-    #predict_number = np.random.randint(n_min, n_max)
+    #predict_number = 1
+    predict_number = np.random.randint(n_min, n_max)
     
     while True: # Выбирам число из середины диапазона, пока не угадаем
         
         count += 1
-        n_mid = round((n_min+n_max)/2) 
-        num = np.random.randint(n_min, n_max)
+        n_mid = round((n_min+n_max) / 2)
+        
+        if n_mid == predict_number:
+            return count
+            
+        else: 
+            if n_mid > predict_number:
+                n_max = n_mid
+            
+            elif n_mid < predict_number:
+                n_min = n_mid
 
+            if n_max - n_min == 1: # Если дошли до n_min=1 и n_min=2
                 
-        if num > predict_number:
-            n_max = n_mid
-            
-        elif num < predict_number:
-            n_min = n_mid
-             
-        if (n_max - n_min) == 1: # Если достигли n_max = 2 и n_min = 1
-            
-            count += 1
-            if n_max == predict_number:
-                
-                return count
-            count += 1
-            if n_min == predict_number:
-                
-                return count   
+                if n_max == predict_number:
+                    count += 1
+                    return count
+                                    
+                if n_min == predict_number:
+                    count += 1
+                    return count
+    
        
 
 def score_game(random_predict_v3) -> int:
@@ -52,7 +53,7 @@ def score_game(random_predict_v3) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    n = 100 # Количество повторений
+    n = 1000 # Количество повторений
 
     for number in range(n):
         count_ls.append(random_predict_v3())
